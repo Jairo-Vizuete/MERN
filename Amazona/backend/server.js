9768f2ff8c2a9c7a7e381f4b772f2de7c9c40697
Jairo-Vizuete <Javp_Tricking@outlook.com>
2021-04-06 22:55:29 -0500

    save

--- Amazona/backend/server.js
+++ Amazona/backend/server.js
@@ -9,23 +9,17 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-  
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
+
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
-
 app.get('/', (req, res) => {
-    res.send('Server is ready');
+  res.send('Server is ready');
 });
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
